87e525eca71ca58e0c8b797f09f19ff48400b247
esmeq <esmeralda.barbieri20@gmail.com>
2026-05-27 12:22:15 +0200

    Project structure + webcam prototype

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sign Language Robot
+
+AI project for real-time sign language recognition.
+
+
+# Sign Language Recognition Robot 🤖
+
+This project aims to develop a real-time system for recognizing sign language gestures using computer vision and machine learning.
+
+## 🔬 Technologies
+- Python
+- OpenCV
+- MediaPipe
+- PyTorch (or TensorFlow)
+- Git/GitHub
+
+## 🎯 Goal
+Real-time recognition of sign language gestures and conversion into text/speech.
+
+## 🧠 Pipeline
+Webcam -> Hand Tracking -> Feature Extraction -> ML Model -> Prediction -> Output
+
+## 📦 Status
+Work in progress (thesis project)
+
+## 👩‍💻 Author
+Esmeralda Barbieri
--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
 print("Project started!")
+
+
+import cv2
+
+def main():
+    cap=cv2.VideoCapture(0)  #con 0 attiva la fotocamera del portatile
+
+    while True:    #ciclo infinito per continuare a raccogliere dati dalla fotocamera
+        ret, frame = cap.read()   #ret è True/False a seconda se legge bene o meno l'immagine
+    
+        if not ret:
+            break
+    
+        cv2.imshow("Sign Language Robot", frame)   #nome finestra e fra= immagine della webcam presa prima
+        if cv2.waitKey(1) & 0xFF== ord('q'):    #se premi q esce dal programma manualmente
+            break
+    cv2.release()   #libera la webcam
+    cv2.destroyAllWindows()   #chiude la finestra
+
+if __name__ == "__main__":
+    main()
